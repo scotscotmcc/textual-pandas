7dd16064069b7bf81e9ac90f0b9f356a5b3edbf2
scot <scot@scotmcc.xyz>
2026-04-11 12:33:16 -0500

    update test docstring

--- tests/test_table.py
+++ tests/test_table.py
@@ -8,7 +8,7 @@ import pandas as pd
 
 @pytest.mark.asyncio
 async def test_dataframetable():
-    "asdf"
+    "Testing creating the DataFrameTable and then later updating it."
     df = pd.DataFrame()
     df["Name"] = ["Dan", "Ben", "Don", "John", "Jim", "Harry"]
     df["Score"] = [77, 56, 90, 99, 83, 69]
